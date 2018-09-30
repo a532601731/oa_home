b30a249fd7662374aa32a25c882c1064b72df38e
Administrator <Administrator@192.168.0.139>
2018-09-30 14:16:48 +0800

    first build

---
+++ src/main/java/com/luchao/controller/controller.java
@@ -0,0 +1,13 @@
+package com.luchao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+	@GetMapping(value="add")
+	public void add(){
+		System.out.println("进入add成功");
+	}
+}
