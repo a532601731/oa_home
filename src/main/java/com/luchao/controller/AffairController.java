97d03ba3cef214556f382657b0d09944a826583f
a532601731 <532601731@qqcom>
2018-10-17 00:27:14 +0800

    完成接收 html，但是无用户数据

--- src/main/java/com/luchao/controller/AffairController.java
+++ src/main/java/com/luchao/controller/AffairController.java
@@ -1,5 +1,7 @@
 package com.luchao.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -27,11 +29,19 @@ public class AffairController {
 	}
 	
 	@GetMapping("add")
-	public String show(Integer affairModuleId,ModelMap modelmap){
-		System.out.println("用户进入了公文管理菜单");
-		
+	public String add(Integer affairModuleId,ModelMap modelmap){
+		System.out.println("用户进入了公文增加菜单");
+		modelmap.addAttribute("affairModuleId",affairModuleId);
 		modelmap.addAttribute("modeloptions",ammos.getByAffairModuleId(affairModuleId));
 		return "affair/add";
 	}
-	
+	@RequestMapping("doadd")
+	public void doadd(HttpServletRequest req){
+		System.out.println("用户进行了公文增加保存操作");
+		System.out.println("affairModuleId:"+req.getParameter("affairModuleId"));
+		
+		System.out.println(req.getParameter("html"));
+		System.out.println("user："+req.getSession().getAttribute("user"));
+		
+	}
 }
---
+++ src/main/java/com/luchao/entity/Affair.java
@@ -0,0 +1,10 @@
+package com.luchao.entity;
+
+public class Affair {
+
+	private Integer affairId;
+	private Integer affairModuleId;
+	private String affairData;
+	private Integer affairUserId;
+	private Integer affairStatus;
+}
